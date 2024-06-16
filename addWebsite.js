ede9cfc7076b7d208c794c47f9d5d1ecb43de4bb
Flexksx <cristian.cretu2808@gmail.com>
2024-06-16 10:54:25 +0300

    mod test for website

--- addWebsite.js
+++ addWebsite.js
@@ -2,7 +2,7 @@ const url = "http://localhost:3000";
 const endpoint = "/thread";
 const thread_id = "thread_T4e3FqCxs0KCfcadzYGUZHjb";
 const requestBody = {
-    "url": "https://xstore.md/setup-pc-gaming/setup-pc-gaming-x01"
+    "url": "https://xstore.md/laptopuri/gaming/asus-rog-strix-g16-g614jz-16-i7-13650hx-32gb-ram-1tb-ssd-rtx4080"
 };
 
 fetch(url + endpoint + "/" + thread_id + "/addwebsite", {
